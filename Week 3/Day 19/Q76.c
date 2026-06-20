e2725467b5074928603cbd6a0a41d9f1bb56322a
satyamshekhar-code <satyamshekhar2019.ss@gmail.com>
2026-06-20 21:35:11 +0530

    Add files via upload

---
+++ Week 3/Day 19/Q76.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10];
+    int n, i, j;
+    int sum = 0;
+
+    printf("Enter the order of the square matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter matrix elements");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        sum += a[i][i];
+    }
+
+    printf("Sum of main diagonal elements = %d", sum);
+
+    return 0;
+}
