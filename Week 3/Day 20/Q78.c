c6260426ac596e9b66ef42e6d85e3521ac86392a
satyamshekhar-code <satyamshekhar2019.ss@gmail.com>
2026-06-24 11:59:18 +0530

    Add files via upload

---
+++ Week 3/Day 20/Q78.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10];
+    int n, i, j, flag = 1;
+
+    printf("Enter the order of the square matrix");
+    scanf("%d", &n);
+
+    printf("Enter matrix elements:");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (a[i][j] != a[j][i]) {
+                flag = 0;
+                break;
+            }
+        }
+    }
+    if (flag)
+        printf("The matrix is symmetric");
+    else
+        printf("The matrix is not symmetric");
+
+    return 0;
+}
